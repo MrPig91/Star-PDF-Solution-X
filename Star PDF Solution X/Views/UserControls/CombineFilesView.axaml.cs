a4dbffe03d4f7a1faf67610a8991e757147ecd48
MrPig91 <sirius.cleveland@gmail.com>
2024-10-05 00:37:07 -0500

    added combinefilesview

---
+++ Star PDF Solution X/Views/UserControls/CombineFilesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Star_PDF_Solution_X;
+
+public partial class CombineFilesView : UserControl
+{
+    public CombineFilesView()
+    {
+        InitializeComponent();
+    }
+}
