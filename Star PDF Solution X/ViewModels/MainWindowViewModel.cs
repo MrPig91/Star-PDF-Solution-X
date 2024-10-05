0943def1265620cafefc7aff99b2a0214ac941de
MrPig91 <sirius.cleveland@gmail.com>
2024-10-05 01:35:28 -0500

    added SplitFileViewModel

--- Star PDF Solution X/ViewModels/MainWindowViewModel.cs
+++ Star PDF Solution X/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using StarPDFSolutionLibrary.Services.Editors;
 
 namespace Star_PDF_Solution_X.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        [ObservableProperty]
-        private string _greeting;
-
+        public CombineFilesViewModel CombineFilesVM { get; }
+        public SplitFileViewModel SplitFileVM { get; }
         public MainWindowViewModel()
         {
-            Greeting = "Hellow World!";
+            IPDFEditorService pdfService = new PDFSharpEditorService();
+            CombineFilesVM = new(pdfService);
+            SplitFileVM = new(pdfService);
         }
     }
 }
