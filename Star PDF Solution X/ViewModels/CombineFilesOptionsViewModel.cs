c90f041e046e1ed9ad55cec07eb4953f756ffb14
MrPig91 <sirius.cleveland@gmail.com>
2024-10-05 01:11:28 -0500

    added options viewmodels

---
+++ Star PDF Solution X/ViewModels/CombineFilesOptionsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_PDF_Solution_X.ViewModels
+{
+    public class CombineFilesOptionsViewModel : ViewModelBase
+    {
+        public CombinePDFOptions GetPDFOptions()
+        {
+            var combineOptions = CombinePDFOptions.None;
+            if (_removeComments)
+                combineOptions = combineOptions.AddFlag(CombinePDFOptions.RemoveComments);
+            if (_removeBookmarks)
+                combineOptions = combineOptions.AddFlag(CombinePDFOptions.RemoveBookmarks);
+            return combineOptions;
+        }
+
+        private bool _openFile;
+        public bool OpenFile
+        {
+            get { return _openFile; }
+            set { _openFile = value; OnPropertyChanged(); }
+        }
+
+        private bool _deleteSourceFiles;
+
+        public bool DeleteSourceFiles
+        {
+            get { return _deleteSourceFiles; }
+            set { _deleteSourceFiles = value; OnPropertyChanged(); }
+        }
+
+        private bool _removeComments;
+        public bool RemoveComments
+        {
+            get => _removeComments;
+            set { _removeComments = value; OnPropertyChanged(); }
+        }
+        private bool _removeBookmarks;
+        public bool RemoveBookmarks
+        {
+            get { return _removeBookmarks; }
+            set { _removeBookmarks = value; }
+        }
+
+        public CombineFilesOptionsViewModel()
+        {
+            OpenFile = true;
+        }
+    }
+}
---
+++ Star PDF Solution X/ViewModels/SplitFileOptionsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_PDF_Solution_X.ViewModels
+{
+    public class SplitFileOptionsViewModel : ViewModelBase
+    {
+        public SplitPDFOptions GetPDFOptions()
+        {
+            var splitOptions = SplitPDFOptions.None;
+            if (_removeComments)
+                splitOptions = splitOptions.AddFlag(SplitPDFOptions.RemoveComments);
+            if (_removeBookmarks)
+                splitOptions = splitOptions.AddFlag(SplitPDFOptions.RemoveBookmarks);
+            return splitOptions;
+        }
+
+        private bool _openDestinationDirectory;
+        public bool OpenDestinationDirectory
+        {
+            get { return _openDestinationDirectory; }
+            set { _openDestinationDirectory = value; OnPropertyChanged(); }
+        }
+        private bool _deleteSourceFile;
+        public bool DeleteSourceFile
+        {
+            get { return _deleteSourceFile; }
+            set { _deleteSourceFile = value; OnPropertyChanged(); }
+        }
+        private bool _removeComments;
+        public bool RemoveComments
+        {
+            get => _removeComments;
+            set { _removeComments = value; OnPropertyChanged(); }
+        }
+        private bool _removeBookmarks;
+        public bool RemoveBookmarks
+        {
+            get { return _removeBookmarks; }
+            set { _removeBookmarks = value; }
+        }
+
+
+        public SplitFileOptionsViewModel()
+        {
+            OpenDestinationDirectory = true;
+            DeleteSourceFile = false;
+            RemoveComments = false;
+        }
+    }
+}
