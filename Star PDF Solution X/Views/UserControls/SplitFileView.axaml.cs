95d736082889620d7df61c927a37757e0c621028
MrPig91 <sirius.cleveland@gmail.com>
2024-10-05 00:37:15 -0500

    added splitfileview

---
+++ Star PDF Solution X/Views/UserControls/SplitFileView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Star_PDF_Solution_X;
+
+public partial class SplitFileView : UserControl
+{
+    public SplitFileView()
+    {
+        InitializeComponent();
+    }
+}
