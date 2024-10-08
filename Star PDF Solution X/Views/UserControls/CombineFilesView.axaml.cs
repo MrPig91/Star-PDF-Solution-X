dc7bd03b0d8333e7064b6c231a98dae77ef54766
MrPig91 <sirius.cleveland@gmail.com>
2024-10-07 20:31:45 -0500

    added drag drop view on combine view

--- Star PDF Solution X/Views/UserControls/CombineFilesView.axaml.cs
+++ Star PDF Solution X/Views/UserControls/CombineFilesView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Star_PDF_Solution_X;
@@ -9,5 +10,43 @@ public partial class CombineFilesView : UserControl
     public CombineFilesView()
     {
         InitializeComponent();
+        AddHandler(DragDrop.DragEnterEvent, Grid_DragEnter);
+        AddHandler(DragDrop.DragLeaveEvent, Grid_DragLeave);
+        combineFilesBorder.AddHandler(DragDrop.DragEnterEvent, dragDropBorder_DragEnter);
+        combineFilesBorder.AddHandler(DragDrop.DragLeaveEvent, dragDropBorder_DragLeave);
+        addFilesBorder.AddHandler(DragDrop.DragEnterEvent, dragDropBorder_DragEnter);
+        addFilesBorder.AddHandler(DragDrop.DragLeaveEvent, dragDropBorder_DragLeave);
+    }
+
+    private void Grid_DragEnter(object? sender, DragEventArgs e)
+    {
+        dragDropGrid.IsVisible = true;
+        addFilesBorder.IsVisible = true;
+        combineFilesBorder.IsVisible = true;
+    }
+    private void dragDropBorder_DragEnter(object? sender, DragEventArgs e)
+    {
+        if (sender is not null && sender is Border)
+        {
+            var border = sender as Border;
+            border.Classes.Remove("DragLeave");
+            border.Classes.Add("DragEnter");
+        }
+    }
+    private void dragDropBorder_DragLeave(object? sender, DragEventArgs e)
+    {
+        if (sender is not null && sender is Border)
+        {
+            var border = sender as Border;
+            border.Classes.Remove("DragEnter");
+            border.Classes.Add("DragLeave");
+        }
+    }
+
+    private void Grid_DragLeave(object? sender, DragEventArgs e)
+    {
+        dragDropGrid.IsVisible = false;
+        addFilesBorder.IsVisible = false;
+        combineFilesBorder.IsVisible = false;
     }
 }
